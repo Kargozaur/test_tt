edcb673a273cd85905d20b93b886304e629e7046
Illia <lpodildaviduk@gmail.com>
2026-04-25 16:28:07 +0200

    engine release

--- services/core/main.py
+++ services/core/main.py
@@ -1,9 +1,20 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+
 from fastapi import APIRouter, FastAPI
 
+from services.core.db.db_conf import engine
 from services.core.routers.router import router
 from shared.middleware.token_middleware import JWTAuthMiddleware
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator:
+    yield
+    await engine.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
 core = APIRouter(prefix="/core")
 core.include_router(router)
 app.include_router(core)
