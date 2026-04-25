a3418b0e38550c5362aefbbc5aaadf8a0f7df185
Illia <lpodildaviduk@gmail.com>
2026-04-25 15:52:27 +0200

    lifespan

--- services/landings/main.py
+++ services/landings/main.py
@@ -7,7 +7,16 @@ from services.landings.routers import lead_router
 from shared.middleware.token_middleware import JWTAuthMiddleware
 from shared.redis_client.client import get_redis_client
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator:
+    redis_client = get_redis_client()
+    app.state.redis = redis_client
+    yield
+    await redis_client.close()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(lead_router.router)
 app.add_middleware(JWTAuthMiddleware)
 
@@ -20,11 +29,3 @@ async def default():
 @app.get("/health")
 async def get_health():
     return {"status": "ready"}
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator:
-    redis_client = get_redis_client()
-    app.state.redis = redis_client
-    yield
-    await redis_client.close()
